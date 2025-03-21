36c1b0a0794a68c62e051ebffecfc1c1c8497722
Zar Ni Myo <zarnimyo@rezeve.com>
2025-03-21 16:34:08 +0630

    first commit

---
+++ example-usecases/Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string bootstrapServers = "host.docker.internal:29092";
+        string topic = "test-topic";
+        string groupId = "test-group";
+
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = bootstrapServers,
+            GroupId = groupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe(topic);
+        Console.WriteLine($"Consumer started...");
+
+        try
+        {
+            while (true)
+            {
+                var consumeResult = consumer.Consume(CancellationToken.None);
+                Console.WriteLine($"Consumed: {consumeResult.Message.Value}");
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            consumer.Close();
+        }
+    }
+}
---
+++ example-usecases/Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using Confluent.Kafka;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string bootstrapServers = "host.docker.internal:29092";
+        string topic = "test-topic";
+
+        var config = new ProducerConfig { BootstrapServers = bootstrapServers };
+
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+        for (int i = 1; i <= 10; i++)
+        {
+            string message = $"Message {i} - {DateTime.Now}";
+            await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+            Console.WriteLine($"Produced: {message}");
+            await Task.Delay(1000);
+        }
+    }
+}
